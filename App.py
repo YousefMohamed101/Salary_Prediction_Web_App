2e2a869945aa31646eb4e312f1ac13aa43de580d
YousefMohamed101 <thehappyboy28@gmail.com>
2026-05-12 07:21:47 +0800

    V0.1
    -added options and organized them

--- App.py
+++ App.py
@@ -2,14 +2,16 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load('catboost_modelV2(89.84).pkl')
+model = joblib.load('Models/catboost_modelV2(89.84).pkl')
 
-st.title('Salary Prediction for IT')
+st.title('Salary Prediction for IT',text_alignment='center')
 
+number = st.slider("Years of experience:", 1, 100, 50)
+country = st.selectbox("Country:", options=['Germany', 'UK', 'USA', 'Canada', 'India', 'France', 'Japan','Australia', 'Brazil', 'Singapore'],key=2)
+education = st.selectbox("Education:", options=['High School', 'Some College', 'Bachelors', 'Masters', 'PhD'],key=3)
+company_size = st.selectbox("Company Size:", options=['1-10' , '11-50', '51-200', '201-1000' , '1001-5000''5000+'],key=4)
+language = st.multiselect("Language:", options=['Python', 'Java', 'Ruby'],key=5)
+framework = st.multiselect("Framework:", options=['Python', 'Java', 'Ruby'],key=6)
 
-exp = st.multiselect("Years of experience:", options=['Python', 'Java', 'Ruby'],max_selections=1,key=1)
-country = st.multiselect("Country:", options=['Python', 'Java', 'Ruby'],key=2)
-education = st.multiselect("Education:", options=['Python', 'Java', 'Ruby'],key=3)
-company_size = st.multiselect("Education:", options=['Python', 'Java', 'Ruby'],key=4)
-
-st.write("You selected:", exp)
+st.button("predict")
+st.write("You selected:", country)
